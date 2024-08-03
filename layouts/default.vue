1e0aa61cdeef25fe4f58686c5e17f54e1f346265
Biamah <beatriz.souza.costa@outlook.com>
2024-08-03 17:17:06 -0300

    Add layout default

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup>
+console.log("show");
+</script>
+
+<template>
+  <div>
+    <p>Some defaulst layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>

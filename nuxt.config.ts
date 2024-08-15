5117e2f37fb40094a3987aed8c4ffbffaaebe723
David Basilio Espitalher <dbasilioesp@gmail.com>
2024-08-15 18:25:56 -0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  ssr: false,
+  ssr: true,
   css: ["~/assets/css/main.css"],
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@nuxt/image"],
   postcss: {},
--- utils/books.js
+++ utils/books.js
@@ -21,7 +21,7 @@ export const books = [
     tags: ["romance", "afrofuturismo", "ficção"],
     recomendamos: true,
     editora: "HarperCollins",
-    slug: "ale-santos",
+    slug: "o-ultimo-ancestral",
     descricao: "",
   },
   {
@@ -29,330 +29,336 @@ export const books = [
     autores: "Andréa Berriell",
     imagem: "/images/livros/Roxo.jpeg",
     recomendamos: true,
+    slug: "roxo",
   },
   {
     nome: "A liberdade é uma luta constante",
     autores: "Angela Davis",
-    imagem: "/images/livros/A liberdade é uma luta constante.jpg",
+    imagem: "/images/livros/A liberdade e uma luta constante.jpg",
     recomendamos: true,
+    slug: "a-liberdade-e-uma-luta-constante",
   },
   {
     nome: "Mulheres, cultura e política",
     autores: "Angela Davis",
-    imagem: "/images/livros/Mulheres, cultura e política.jpg",
+    imagem: "/images/livros/Mulheres, cultura e politica.jpg",
+    slug: "mulheres-cultura-politica",
   },
   {
     nome: "O sentido da liberdade",
     autores: "Angela Davis",
     imagem: "/images/livros/O sentido da liberdade.png",
+    slug: "o-sentido-da-liberdade",
   },
   {
     nome: "O pequeno príncipe",
     autores: "Antoine de saint-exupéry",
-    imagem: "/images/livros/O pequeno príncipe.jpg",
+    imagem: "/images/livros/O pequeno principe.jpg",
+    slug: "o-pequeno-principe",
   },
   {
     nome: "Drácula",
     autores: "Bram Stoker",
     imagem: "/images/livros/Dracula.png",
-  },
-  {
-    nome: "As aventuras de pinóquio",
-    autores: "Carlo Collodi",
-    imagem: "/images/livros/As aventuras de pinóquio.png",
-  },
-  {
-    nome: "O escravo",
-    autores: "Carolina Maria de Jesus",
-    imagem: "/images/livros/O escravo.jpg",
-  },
-  {
-    nome: "Vermelho, branco e sangue azul",
-    autores: "Casey McQuiston",
-    imagem: "/images/livros/Vermelho, branco e sangue azul.jpg",
-  },
-  {
-    nome: "O Código Da Vinci",
-    autores: "Dan Brown",
-    imagem: "/images/livros/O Código Da Vinci.jpg",
-  },
-  {
-    nome: "A floresta",
-    autores: "Daniel Gruber",
-    imagem: "/images/livros/AFloresta.jpeg",
-  },
-  {
-    nome: "A noite do cordeiro",
-    autores: "Daniel Gruber",
-    imagem: "/images/livros/ANoiteDoCordeiro.jpeg",
-  },
-  {
-    nome: "O novo horror",
-    autores: "Daniel Gruber, Irka Barrios",
-    imagem: "/images/livros/NovoHorror.jpeg",
-  },
-  {
-    nome: "Colonialismo digital",
-    autores: "Deivison Faustino, Walter Lippold",
-    imagem: "/images/livros/Colonialismo digital.webp",
-  },
-  {
-    nome: "O pequeno manual antirracista",
-    autores: "Djamila Ribeiro",
-    imagem: "/images/livros/O pequeno manual antirracista.jpg",
-  },
-  {
-    nome: "A Vida, o Universo e Tudo Mais - Mochileiro das Galáxias - Vol. 3",
-    autores: "Douglas Adams",
-    imagem:
-      "/images/livros/A Vida, o Universo e Tudo Mais - Mochileiro das Galáxias - Vol. 3.jpg",
-  },
-  {
-    nome: "Até mais, e obrigado pelos peixes! - Vol. 4",
-    autores: "Douglas Adams",
-    imagem: "/images/livros/Até mais, e obrigado pelos peixes! - Vol. 4.jpg",
-  },
-  {
-    nome: "O guia do mochileiro das galáxias",
-    autores: "Douglas adams",
-    imagem: "/images/livros/O guia do mochileiro das galáxias.jpg",
-  },
-  {
-    nome: "Praticamente Inofensiva",
-    autores: "Douglas Adams",
-  },
-  {
-    nome: "Vê se cresce",
-    autores: "Eve brown, Talia hibbert",
-  },
-  {
-    nome: "O grande gatsby",
-    autores: "F. Scott fitzgerald",
-  },
-  {
-    nome: "Confissões perdidas",
-    autores: "Fernando Mantelli",
-  },
-  {
-    nome: "Crime castigo",
-    autores: "Fiódor dostoiévski",
-  },
-  {
-    nome: "Duna",
-    autores: "Frank Herbert",
-  },
-  {
-    nome: "Onze estreias",
-    autores: "Fred Linardi",
-  },
-  {
-    nome: "1984",
-    autores: "George Orwell",
-  },
-  {
-    nome: "Vidas secas",
-    autores: "Graciliano Ramos",
-  },
-  {
-    nome: "Biblioteca H.P. Lovecraft - O chamado de cthulhu e outras histórias",
-    autores: "H.P. Lovecraft",
-  },
-  {
-    nome: "Os números do amor",
-    autores: "Helen Hoang",
-  },
-  {
-    nome: "A rainha do nada",
-    autores: "Holly Black",
-  },
-  {
-    nome: "O herdeiro roubado",
-    autores: "Holly black",
-  },
-  {
-    nome: "O principe cruel",
-    autores: "Holly Black",
-  },
-  {
-    nome: "O rei perverso",
-    autores: "Holly Black",
-  },
-  {
-    nome: "Torto arado",
-    autores: "Itamar Vieira Junior",
-  },
-  {
-    nome: "Harry potter e a câmera secreta",
-    autores: "J.K. Rowling",
-  },
-  {
-    nome: "Harry Potter e a criança amaldiçoada",
-    autores: "J.K. Rowling",
-  },
-  {
-    nome: "Harry Potter e a pedra filosofal",
-    autores: "J.K. Rowling",
-  },
-  {
-    nome: "Harry Potter e o Cálice de Fogo",
-    autores: "J.K. Rowling",
-  },
-  {
-    nome: "Harry potter e o prisioneiro de azkaban",
-    autores: "J.K. Rowling",
-  },
-  {
-    nome: "O senhor dos anéis - Parte 1",
-    autores: "J.R.R. Tolkien",
-  },
-  {
-    nome: "O senhor dos anéis - Parte 2 - As duas torres",
-    autores: "J.R.R. Tolkien",
-  },
-  {
-    nome: "O senhor dos anéis - Parte 3 - O retorno do rei",
-    autores: "J.R.R. Tolkien",
-  },
-  {
-    nome: "Agora e para sempre Lara jean",
-    autores: "Jenny Han",
-  },
-  {
-    nome: "P.S.: Ainda amo você",
-    autores: "Jenny Han",
-  },
-  {
-    nome: "Para todos os garotos que já amei",
-    autores: "Jenny Han",
-  },
-  {
-    nome: "O serviço de entregas monstruosas",
-    autores: "Jim Anotsu (Gustavo)",
-  },
-  {
-    nome: "A culpa é das estrelas",
-    autores: "John Green",
-  },
-  {
-    nome: "Cidades de papel",
-    autores: "John Green",
-  },
-  {
-    nome: "O teorema katherine",
-    autores: "John Green",
-  },
-  {
-    nome: "Deixe a neve cair",
-    autores: "John Green, Maureen Johnson, Lauren Myracle",
-  },
-  {
-    nome: "Ainda sou eu",
-    autores: "Jojo Moyes",
-  },
-  {
-    nome: "Depois de você",
-    autores: "Jojo Moyes",
-  },
-  {
-    nome: "Um caso sem futuro",
-    autores: "Júlia Maizman",
-    imagem: "/images/livros/UmCasoSemFuturo.png",
-  },
-  {
-    nome: "A rede de Alice",
-    autores: "Kate Quinn",
-  },
-  {
-    nome: "De lá pra cá",
-    autores: "Laís Loffredo",
-  },
-  {
-    nome: "Por um feminismo afro latino americano",
-    autores: "Lélia Gonzalez",
-  },
-  {
-    nome: "O inimigo do mundo",
-    autores: "Leonel Caldela",
-  },
-  {
-    nome: "Alice no país das maravilhas",
-    autores: "Lewis carroll",
-  },
-  {
-    nome: "Gwen & Art não estão apaixonados",
-    autores: "Lex Croucher",
-  },
-  {
-    nome: "As guerras do mundo emerso",
-    autores: "Licia Troisi",
-  },
-  {
-    nome: "Os filhos da terra e outros contos",
-    autores: "Lucas Lopes",
-  },
-  {
-    nome: "Encontro dos Orixás",
-    autores: "Maria Aparecida Martins",
-  },
-  {
-    nome: "A biblioteca da meia noite",
-    autores: "Matt Haig",
-  },
-  {
-    nome: "Água turva",
-    autores: "Morgana Kretzmann",
-  },
-  {
-    nome: "Alerta de Spoiler",
-    autores: "Olivia Dade",
-  },
-  {
-    nome: "30 Poemas de um negro brasileiro",
-    autores: "Oswaldo de camargo",
-  },
-  {
-    nome: "O alquimista",
-    autores: "Paulo Celho",
-  },
-  {
-    nome: "As valkírias",
-    autores: "Paulo Coelho",
-  },
-  {
-    nome: "Brida",
-    autores: "Paulo Coelho",
-  },
-  {
-    nome: "O alquimista",
-    autores: "Paulo Coelho",
-  },
-  {
-    nome: "O diário de um mago",
-    autores: "Paulo Coelho",
-  },
-  {
-    nome: "O monte cinco",
-    autores: "Paulo Coelho",
-  },
-  {
-    nome: "A herença dos fantasmas",
-    autores: "Rivers Solomon",
-    imagem: "/images/livros/AHerançaDosFantasmas.png",
-  },
-  {
-    nome: "A pequena caixa de gwendy",
-    autores: "Stephen King",
-  },
-  {
-    nome: "It a coisa",
-    autores: "Stephen King",
-  },
-  {
-    nome: "Os sete maridos de Evelyn hugo",
-    autores: "Taylor Jenkins Reid",
-  },
-  {
-    nome: "A filha primitiva",
-    autores: "Vanessa Passos",
-  },
+    slug: "dracula",
+  },
+  // {
+  //   nome: "As aventuras de pinóquio",
+  //   autores: "Carlo Collodi",
+  //   imagem: "/images/livros/As aventuras de pinóquio.png",
+  // },
+  // {
+  //   nome: "O escravo",
+  //   autores: "Carolina Maria de Jesus",
+  //   imagem: "/images/livros/O escravo.jpg",
+  // },
+  // {
+  //   nome: "Vermelho, branco e sangue azul",
+  //   autores: "Casey McQuiston",
+  //   imagem: "/images/livros/Vermelho, branco e sangue azul.jpg",
+  // },
+  // {
+  //   nome: "O Código Da Vinci",
+  //   autores: "Dan Brown",
+  //   imagem: "/images/livros/O Código Da Vinci.jpg",
+  // },
+  // {
+  //   nome: "A floresta",
+  //   autores: "Daniel Gruber",
+  //   imagem: "/images/livros/AFloresta.jpeg",
+  // },
+  // {
+  //   nome: "A noite do cordeiro",
+  //   autores: "Daniel Gruber",
+  //   imagem: "/images/livros/ANoiteDoCordeiro.jpeg",
+  // },
+  // {
+  //   nome: "O novo horror",
+  //   autores: "Daniel Gruber, Irka Barrios",
+  //   imagem: "/images/livros/NovoHorror.jpeg",
+  // },
+  // {
+  //   nome: "Colonialismo digital",
+  //   autores: "Deivison Faustino, Walter Lippold",
+  //   imagem: "/images/livros/Colonialismo digital.webp",
+  // },
+  // {
+  //   nome: "O pequeno manual antirracista",
+  //   autores: "Djamila Ribeiro",
+  //   imagem: "/images/livros/O pequeno manual antirracista.jpg",
+  // },
+  // {
+  //   nome: "A Vida, o Universo e Tudo Mais - Mochileiro das Galáxias - Vol. 3",
+  //   autores: "Douglas Adams",
+  //   imagem:
+  //     "/images/livros/A Vida, o Universo e Tudo Mais - Mochileiro das Galáxias - Vol. 3.jpg",
+  // },
+  // {
+  //   nome: "Até mais, e obrigado pelos peixes! - Vol. 4",
+  //   autores: "Douglas Adams",
+  //   imagem: "/images/livros/Até mais, e obrigado pelos peixes! - Vol. 4.jpg",
+  // },
+  // {
+  //   nome: "O guia do mochileiro das galáxias",
+  //   autores: "Douglas adams",
+  //   imagem: "/images/livros/O guia do mochileiro das galáxias.jpg",
+  // },
+  // {
+  //   nome: "Praticamente Inofensiva",
+  //   autores: "Douglas Adams",
+  // },
+  // {
+  //   nome: "Vê se cresce",
+  //   autores: "Eve brown, Talia hibbert",
+  // },
+  // {
+  //   nome: "O grande gatsby",
+  //   autores: "F. Scott fitzgerald",
+  // },
+  // {
+  //   nome: "Confissões perdidas",
+  //   autores: "Fernando Mantelli",
+  // },
+  // {
+  //   nome: "Crime castigo",
+  //   autores: "Fiódor dostoiévski",
+  // },
+  // {
+  //   nome: "Duna",
+  //   autores: "Frank Herbert",
+  // },
+  // {
+  //   nome: "Onze estreias",
+  //   autores: "Fred Linardi",
+  // },
+  // {
+  //   nome: "1984",
+  //   autores: "George Orwell",
+  // },
+  // {
+  //   nome: "Vidas secas",
+  //   autores: "Graciliano Ramos",
+  // },
+  // {
+  //   nome: "Biblioteca H.P. Lovecraft - O chamado de cthulhu e outras histórias",
+  //   autores: "H.P. Lovecraft",
+  // },
+  // {
+  //   nome: "Os números do amor",
+  //   autores: "Helen Hoang",
+  // },
+  // {
+  //   nome: "A rainha do nada",
+  //   autores: "Holly Black",
+  // },
+  // {
+  //   nome: "O herdeiro roubado",
+  //   autores: "Holly black",
+  // },
+  // {
+  //   nome: "O principe cruel",
+  //   autores: "Holly Black",
+  // },
+  // {
+  //   nome: "O rei perverso",
+  //   autores: "Holly Black",
+  // },
+  // {
+  //   nome: "Torto arado",
+  //   autores: "Itamar Vieira Junior",
+  // },
+  // {
+  //   nome: "Harry potter e a câmera secreta",
+  //   autores: "J.K. Rowling",
+  // },
+  // {
+  //   nome: "Harry Potter e a criança amaldiçoada",
+  //   autores: "J.K. Rowling",
+  // },
+  // {
+  //   nome: "Harry Potter e a pedra filosofal",
+  //   autores: "J.K. Rowling",
+  // },
+  // {
+  //   nome: "Harry Potter e o Cálice de Fogo",
+  //   autores: "J.K. Rowling",
+  // },
+  // {
+  //   nome: "Harry potter e o prisioneiro de azkaban",
+  //   autores: "J.K. Rowling",
+  // },
+  // {
+  //   nome: "O senhor dos anéis - Parte 1",
+  //   autores: "J.R.R. Tolkien",
+  // },
+  // {
+  //   nome: "O senhor dos anéis - Parte 2 - As duas torres",
+  //   autores: "J.R.R. Tolkien",
+  // },
+  // {
+  //   nome: "O senhor dos anéis - Parte 3 - O retorno do rei",
+  //   autores: "J.R.R. Tolkien",
+  // },
+  // {
+  //   nome: "Agora e para sempre Lara jean",
+  //   autores: "Jenny Han",
+  // },
+  // {
+  //   nome: "P.S.: Ainda amo você",
+  //   autores: "Jenny Han",
+  // },
+  // {
+  //   nome: "Para todos os garotos que já amei",
+  //   autores: "Jenny Han",
+  // },
+  // {
+  //   nome: "O serviço de entregas monstruosas",
+  //   autores: "Jim Anotsu (Gustavo)",
+  // },
+  // {
+  //   nome: "A culpa é das estrelas",
+  //   autores: "John Green",
+  // },
+  // {
+  //   nome: "Cidades de papel",
+  //   autores: "John Green",
+  // },
+  // {
+  //   nome: "O teorema katherine",
+  //   autores: "John Green",
+  // },
+  // {
+  //   nome: "Deixe a neve cair",
+  //   autores: "John Green, Maureen Johnson, Lauren Myracle",
+  // },
+  // {
+  //   nome: "Ainda sou eu",
+  //   autores: "Jojo Moyes",
+  // },
+  // {
+  //   nome: "Depois de você",
+  //   autores: "Jojo Moyes",
+  // },
+  // {
+  //   nome: "Um caso sem futuro",
+  //   autores: "Júlia Maizman",
+  //   imagem: "/images/livros/UmCasoSemFuturo.png",
+  // },
+  // {
+  //   nome: "A rede de Alice",
+  //   autores: "Kate Quinn",
+  // },
+  // {
+  //   nome: "De lá pra cá",
+  //   autores: "Laís Loffredo",
+  // },
+  // {
+  //   nome: "Por um feminismo afro latino americano",
+  //   autores: "Lélia Gonzalez",
+  // },
+  // {
+  //   nome: "O inimigo do mundo",
+  //   autores: "Leonel Caldela",
+  // },
+  // {
+  //   nome: "Alice no país das maravilhas",
+  //   autores: "Lewis carroll",
+  // },
+  // {
+  //   nome: "Gwen & Art não estão apaixonados",
+  //   autores: "Lex Croucher",
+  // },
+  // {
+  //   nome: "As guerras do mundo emerso",
+  //   autores: "Licia Troisi",
+  // },
+  // {
+  //   nome: "Os filhos da terra e outros contos",
+  //   autores: "Lucas Lopes",
+  // },
+  // {
+  //   nome: "Encontro dos Orixás",
+  //   autores: "Maria Aparecida Martins",
+  // },
+  // {
+  //   nome: "A biblioteca da meia noite",
+  //   autores: "Matt Haig",
+  // },
+  // {
+  //   nome: "Água turva",
+  //   autores: "Morgana Kretzmann",
+  // },
+  // {
+  //   nome: "Alerta de Spoiler",
+  //   autores: "Olivia Dade",
+  // },
+  // {
+  //   nome: "30 Poemas de um negro brasileiro",
+  //   autores: "Oswaldo de camargo",
+  // },
+  // {
+  //   nome: "O alquimista",
+  //   autores: "Paulo Celho",
+  // },
+  // {
+  //   nome: "As valkírias",
+  //   autores: "Paulo Coelho",
+  // },
+  // {
+  //   nome: "Brida",
+  //   autores: "Paulo Coelho",
+  // },
+  // {
+  //   nome: "O alquimista",
+  //   autores: "Paulo Coelho",
+  // },
+  // {
+  //   nome: "O diário de um mago",
+  //   autores: "Paulo Coelho",
+  // },
+  // {
+  //   nome: "O monte cinco",
+  //   autores: "Paulo Coelho",
+  // },
+  // {
+  //   nome: "A herença dos fantasmas",
+  //   autores: "Rivers Solomon",
+  //   imagem: "/images/livros/AHerançaDosFantasmas.png",
+  // },
+  // {
+  //   nome: "A pequena caixa de gwendy",
+  //   autores: "Stephen King",
+  // },
+  // {
+  //   nome: "It a coisa",
+  //   autores: "Stephen King",
+  // },
+  // {
+  //   nome: "Os sete maridos de Evelyn hugo",
+  //   autores: "Taylor Jenkins Reid",
+  // },
+  // {
+  //   nome: "A filha primitiva",
+  //   autores: "Vanessa Passos",
+  // },
 ];
